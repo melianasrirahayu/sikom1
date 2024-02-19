2f5f3efcd4a98ff2f5817a2f89d351243d214d22
melianasrirahayu <melianasri@gmail.com>
2024-02-19 15:09:43 +0700

    first commit meliana

---
+++ app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+    protected $guarded = ['id'];
+
+    public function peminjaman()
+    {
+        return $this->hasMany(Peminjaman::class);
+    }
+
+    public function koleksipribadi()
+    {
+        return $this->hasMany(Koleksipiribadi::class);
+    }
+
+    public function ulasanbuku()
+    {
+        return $this->hasMany(Ulasanbuku::class);
+    }
+}
---
+++ resources/views/_templat_back/sidebar.blade.php
@@ -0,0 +1,275 @@
+<div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
+			<div class="sticky">
+				<aside class="app-sidebar sidebar-scroll">
+					<div class="main-sidebar-header active">
+						<a class="desktop-logo logo-light active" href="index.html"><img src="{{assets('assets')}}/img/brand/logo.png" class="main-logo" alt="logo"></a>
+						<a class="desktop-logo logo-dark active" href="index.html"><img src="{{assets('assets')}}/img/brand/logo-white.png" class="main-logo" alt="logo"></a>
+						<a class="logo-icon mobile-logo icon-light active" href="index.html"><img src="{{assets('assets')}}/img/brand/favicon.png" alt="logo"></a>
+						<a class="logo-icon mobile-logo icon-dark active" href="index.html"><img src="{{assets('assets')}}/img/brand/favicon-white.png" alt="logo"></a>
+					</div>
+					<div class="main-sidemenu">
+						<div class="main-sidebar-loggedin">
+							<div class="app-sidebar__user">
+								<div class="dropdown user-pro-body text-center">
+									<div class="user-pic">
+										<img src="{{assets('assets')}}/img/faces/6.jpg" alt="user-img" class="rounded-circle mCS_img_loaded">
+									</div>
+									<div class="user-info">
+										<h6 class=" mb-0 text-dark">Petey Cruiser</h6>
+										<span class="text-muted app-sidebar__user-name text-sm">Administrator</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="sidebar-navs">
+							<ul class="nav  nav-pills-circle">
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Settings" aria-describedby="tooltip365540">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-settings"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Chat" aria-describedby="tooltip143427">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-mail"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Followers">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-user"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Logout">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-power"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"/></svg></div>
+						<ul class="side-menu ">
+							<li class="slide">
+								<a class="side-menu__item" href="index.html"><i class="side-menu__icon fe fe-airplay"></i><span class="side-menu__label">Dashboard</span></a>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-box"></i><span class="side-menu__label">Apps</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Apps</a></li>
+									<li><a class="slide-item" href="cards.html">Cards</a></li>
+									<li><a class="slide-item" href="darggablecards.html">Darggablecards</a></li>
+									<li><a class="slide-item" href="rangeslider.html">Range-slider</a></li>
+									<li><a class="slide-item" href="calendar.html">Calendar</a></li>
+									<li><a class="slide-item" href="contacts.html">Contacts</a></li>
+									<li><a class="slide-item" href="image-compare.html">Image-compare</a></li>
+									<li><a class="slide-item" href="notification.html">Notification</a></li>
+									<li><a class="slide-item" href="widget-notification.html">Widget-notification</a></li>
+									<li><a class="slide-item" href="treeview.html">Treeview</a></li>
+									<li><a class="slide-item" href="widgets.html">Widgets</a></li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">Mail</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="mail.html">Mail</a></li>
+											<li><a class="sub-side-menu__item" href="mail-compose.html">Mail Compose</a></li>
+											<li><a class="sub-side-menu__item" href="mail-read.html">Read-mail</a></li>
+											<li><a class="sub-side-menu__item" href="mail-settings.html">mail-settings</a></li>
+											<li><a class="sub-side-menu__item" href="chat.html">Chat</a></li>
+										</ul>
+									</li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">Maps</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="map-leaflet.html">Mapel Maps</a></li>
+											<li><a class="sub-side-menu__item" href="map-vector.html">Vector Maps</a></li>
+										</ul>
+									</li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">File-Manager</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="file-manager.html">File-Manager</a></li>
+											<li><a class="sub-side-menu__item" href="file-manager-list.html">File-Manager-List</a></li>
+											<li><a class="sub-side-menu__item" href="file-manager-details.html">File-Manager-Details</a></li>
+											<li><a class="sub-side-menu__item" href="file-attachments.html">File-Attachments</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-award "></i><span class="side-menu__label">Icons</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Icons</a></li>
+									<li><a class="slide-item" href="icons.html">Font Awesome </a></li>
+									<li><a class="slide-item" href="icons-2.html">Material Design Icons</a></li>
+									<li><a class="slide-item" href="icons-3.html">Simple Line Icons</a></li>
+									<li><a class="slide-item" href="icons-4.html">Feather Icons</a></li>
+									<li><a class="slide-item" href="icons-5.html">Ionic Icons</a></li>
+									<li><a class="slide-item" href="icons-6.html">Flag Icons</a></li>
+									<li><a class="slide-item" href="icons-7.html">Pe7 Icons</a></li>
+									<li><a class="slide-item" href="icons-8.html">Themify Icons</a></li>
+									<li><a class="slide-item" href="icons-9.html">Typicons Icons</a></li>
+									<li><a class="slide-item" href="icons-10.html">Weather Icons</a></li>
+									<li><a class="slide-item" href="icons-11.html">Material Icons</a></li>
+									<li><a class="slide-item" href="icons-12.html">Bootstrap Icons</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-layers "></i><span class="side-menu__label">Elements</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Elements</a></li>
+									<li><a class="slide-item" href="alerts.html">Alerts</a></li>
+									<li><a class="slide-item" href="avatar.html">Avatar</a></li>
+									<li><a class="slide-item" href="breadcrumbs.html">Breadcrumbs</a></li>
+									<li><a class="slide-item" href="buttons.html">Buttons</a></li>
+									<li><a class="slide-item" href="badge.html">Badge</a></li>
+									<li><a class="slide-item" href="dropdown.html">Dropdown</a></li>
+									<li><a class="slide-item" href="thumbnails.html">Thumbnails</a></li>
+									<li><a class="slide-item" href="images.html">Images</a></li>
+									<li><a class="slide-item" href="list-group.html">List Group</a></li>
+									<li><a class="slide-item" href="navigation.html">Navigation</a></li>
+									<li><a class="slide-item" href="pagination.html">Pagination</a></li>
+									<li><a class="slide-item" href="popover.html">Popover</a></li>
+									<li><a class="slide-item" href="progress.html">Progress</a></li>
+									<li><a class="slide-item" href="spinners.html">Spinners</a></li>
+									<li><a class="slide-item" href="media-object.html">Media Object</a></li>
+									<li><a class="slide-item" href="typography.html">Typography</a></li>
+									<li><a class="slide-item" href="tooltip.html">Tooltip</a></li>
+									<li><a class="slide-item" href="toast.html">Toast</a></li>
+									<li><a class="slide-item" href="tags.html">Tags</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-package "></i><span class="side-menu__label">Advanced UI</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Advanced Ui</a></li>
+									<li><a class="slide-item" href="accordion.html">Accordion</a></li>
+									<li><a class="slide-item" href="carousel.html">Carousel</a></li>
+									<li><a class="slide-item" href="collapse.html">Collapse</a></li>
+									<li><a class="slide-item" href="modals.html">Modals</a></li>
+									<li><a class="slide-item" href="timeline.html">Timeline</a></li>
+									<li><a class="slide-item" href="sweet-alert.html">Sweet Alert</a></li>
+									<li><a class="slide-item" href="rating.html">Ratings</a></li>
+									<li><a class="slide-item" href="counters.html">Counters</a></li>
+									<li><a class="slide-item" href="search.html">Search</a></li>
+									<li><a class="slide-item" href="userlist.html">Userlist</a></li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">Blog-Pages</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="blog.html">Blog</a></li>
+											<li><a class="sub-side-menu__item" href="blog-details.html">Blog-Details</a></li>
+											<li><a class="sub-side-menu__item" href="edit-post.html">Blog-Edit</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-file"></i><span class="side-menu__label">Forms</span><span class="badge bg-info side-badge">6</span><i class="angle fe fe-chevron-down  hor-angle"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Forms</a></li>
+									<li><a class="slide-item" href="form-elements.html">Form Elements</a></li>
+									<li><a class="slide-item" href="form-advanced.html">Advanced Forms</a></li>
+									<li><a class="slide-item" href="form-layouts.html">Form Layouts</a></li>
+									<li><a class="slide-item" href="form-validation.html">Form Validation</a></li>
+									<li><a class="slide-item" href="form-wizards.html">Form Wizards</a></li>
+									<li><a class="slide-item" href="form-editor.html">WYSIWYG Editor</a></li>
+									<li><a class="slide-item" href="form-sizes.html">Form-Sizes</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-bar-chart-2 "></i><span class="side-menu__label">Charts</span><span class="badge bg-danger side-badge">5</span><i class="angle fe fe-chevron-down hor-angle"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Charts</a></li>
+									<li><a class="slide-item" href="chart-morris.html">Morris Charts</a></li>
+									<li><a class="slide-item" href="chart-flot.html">Flot Charts</a></li>
+									<li><a class="slide-item" href="chart-chartjs.html">ChartJS</a></li>
+									<li><a class="slide-item" href="chart-echart.html">Echart</a></li>
+									<li><a class="slide-item" href="chart-sparkline.html">Sparkline</a></li>
+									<li><a class="slide-item" href="chart-peity.html">Chart-peity</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-compass"></i><span class="side-menu__label">Pages</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Pages</a></li>
+									<li><a class="slide-item" href="profile.html">Profile</a></li>
+									<li><a class="slide-item" href="editprofile.html">Edit-Profile</a></li>
+									<li><a class="slide-item" href="about.html">About-Us</a></li>
+									<li><a class="slide-item" href="settings.html">Settings</a></li>
+									<li><a class="slide-item" href="invoice.html">Invoice</a></li>
+									<li><a class="slide-item" href="pricing.html">Pricing</a></li>
+									<li><a class="slide-item" href="gallery.html">Gallery</a></li>
+									<li><a class="slide-item" href="todotask.html">Todotask</a></li>
+									<li><a class="slide-item" href="faq.html">Faqs</a></li>
+									<li><a class="slide-item" href="empty.html">Empty Page</a></li>
+									<li><a class="slide-item" href="Switcher-1.html">switcher-1</a></li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">Tables</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="table-basic.html">Basic Tables</a></li>
+											<li><a class="sub-side-menu__item" href="table-data.html">Data Tables</a></li>
+											<li><a class="sub-side-menu__item" href="edit-table.html">Edit Tables</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-grid"></i><span class="side-menu__label">Submenus</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li class="side-menu__label1"><a href="javascript:void(0);">Submenus</a></li>
+									<li><a class="slide-item"   href="javascript:void(0);">Level1</a></li>
+									<li class="sub-slide">
+										<a class="slide-item" data-bs-toggle="sub-slide" href="javascript:void(0);"><span class="sub-side-menu__label">Level-2</span><i class="sub-angle fe fe-chevron-down"></i></a>
+										<ul class="sub-slide-menu">
+											<li><a class="sub-side-menu__item" href="javascript:void(0);">Level-2.1</a></li>
+											<li class="sub-slide2">
+												<a class="sub-side-menu__item" data-bs-toggle="sub-slide2" href="javascript:void(0);"><span class="sub-side-menu__label">Level-2.2</span><i class="sub-angle2 fe fe-chevron-down"></i></a>
+												<ul class="sub-slide-menu1">
+													<li><a class="sub-slide-item2" href="javascript:void(0);">Level-2.2.1</a></li>
+													<li><a class="sub-slide-item2" href="javascript:void(0);">Level-2.2.2</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-shopping-cart"></i><span class="side-menu__label">Ecommerce</span><span class="badge bg-success side-badge">3</span><i class="angle fe fe-chevron-down  hor-angle"></i></a>
+								<ul class="slide-menu">
+									<li><a class="slide-item" href="products.html">Products</a></li>
+									<li><a class="slide-item" href="product-details.html">Product-Details</a></li>
+									<li><a class="slide-item" href="product-cart.html">Cart</a></li>
+									<li><a class="slide-item" href="check-out.html">Check-out</a></li>
+									<li><a class="slide-item" href="wish-list.html">Wish-list</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-codepen "></i><span class="side-menu__label">Utilities</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li><a class="slide-item" href="background.html">Background</a></li>
+									<li><a class="slide-item" href="border.html">Border</a></li>
+									<li><a class="slide-item" href="display.html">Display</a></li>
+									<li><a class="slide-item" href="flex.html">Flex</a></li>
+									<li><a class="slide-item" href="height.html">Height</a></li>
+									<li><a class="slide-item" href="margin.html">Margin</a></li>
+									<li><a class="slide-item" href="padding.html">Padding</a></li>
+									<li><a class="slide-item" href="position.html">Position</a></li>
+									<li><a class="slide-item" href="width.html">Width</a></li>
+									<li><a class="slide-item" href="extras.html">Extras</a></li>
+								</ul>
+							</li>
+							<li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="javascript:void(0);"><i class="side-menu__icon fe fe-aperture"></i><span class="side-menu__label">Custom Pages</span><i class="angle fe fe-chevron-down"></i></a>
+								<ul class="slide-menu">
+									<li><a class="slide-item" href="signin.html">Sign In</a></li>
+									<li><a class="slide-item" href="signup.html">Sign Up</a></li>
+									<li><a class="slide-item" href="forgot.html">Forgot Password</a></li>
+									<li><a class="slide-item" href="reset.html">Reset Password</a></li>
+									<li><a class="slide-item" href="lockscreen.html">Lockscreen</a></li>
+									<li><a class="slide-item" href="underconstruction.html">UnderConstruction</a></li>
+									<li><a class="slide-item" href="404.html">404 Error</a></li>
+									<li><a class="slide-item" href="500.html">500 Error</a></li>
+								</ul>
+							</li>
+						</ul>
+
+						<div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"/></svg></div>
+					</div>
+				</aside>
+			</div>
